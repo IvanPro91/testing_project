2a965074346f5a546dfe95776dfc935fe99be144
Иван <ivan-don4encko@ya.ru>
2024-03-01 10:28:32 +0300

    Создание тестового проекта

--- website/__init__.py
+++ website/__init__.py
@@ -1,22 +1,31 @@
+import os
+from instance.config import *
 from flask import Flask, make_response
-from sqlalchemy import NullPool
 from .extensions import *
 from .includes.database.database_models import *
 from .routes.authorization import *
 from flask_restful import Api
 
+def load_config(mode=os.environ.get('MODE')):
+    """Выбор и загрузка конфигурации."""
+    print(mode)
+    try:
+        if mode:
+            if 'prod' in mode.lower():
+                return ProductionConfig
+            elif 'dev' in mode.lower():
+                return DevelopmentConfig
+        else:
+            return None
+    except ImportError:
+        return None
+
+
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+
     # Конфигурации
-    app.secret_key = "agag-sgasasf14112asfafh1sfgksafgkw-wq45ufrjqwrtj"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:2222@localhost:5432/TestingProject"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-    app.config['SQLALCHEMY_POOL_SIZE'] = 20  # Количество соединений в пуле
-    app.config['SQLALCHEMY_POOL_TIMEOUT'] = 10  # Время ожидания соединения в секундах
-    app.config['SQLALCHEMY_POOL_RECYCLE'] = 3600  # Время переиспользования соединения в секундах
-    app.config['SCHEDULER_API_INTERVAL'] = 5  # in seconds
-    app.config['SQLALCHEMY_POOL_CLASS'] = NullPool
+    app.config.from_object(load_config())
 
     # Подключаем маршруты
     api = Api(app, prefix="/api/v1")
